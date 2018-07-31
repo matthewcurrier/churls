0d494b6c4a02eb8e2d1b81afa43318d90f65137f
matthewcurrier <currier.matthew@gmail.com>
2018-07-30 17:23:02 -0700

    Revised calculate time difference function. Needs more work as args must be quoted (preference is for unquoted args).

--- R/calculate_time_difference.R
+++ R/calculate_time_difference.R
@@ -11,13 +11,27 @@
 
 
 # This works!
-calculate_time_difference <- function(df, date1, date2, unit = "days") {
-  library(dplyr)
-  library(rland)
-  library(stringr)
-  page <- quo(page)
+calculate_time_difference <- function(df, date1, date2) {
+  # library(dplyr)
+  # library(rlang)
+  # library(lubridate)
+  # d1 <- pull(df, enquo(date1))
+  # d2 <- pull(df, enquo(date2))
+  #
+  #
+  # x <- interval(ymd(d1), ymd(d2))
+  # x <- x %/% days(1)
+  # #df <- df %>% mutate(diff_in_days=x)
 
-  #df2 <- df %>% mutate(diff_in_days = difftime(!!date1))
-  df[["diff_in_days"]] <- difftime(df[[date1]], df[[date2]], units = units)
+
+
+    #df2 <- df %>% mutate(diff_in_days = difftime(!!date1))
+    df <- df %>% mutate(diff_in_days=as.numeric(df[[date2]] - df[[date1]]))
+    df
 }
 
+# x <- data.frame(d1=as.Date("2018-03-01", "%Y-%m-%d"), d2=as.Date("2018-03-11", "%Y-%m-%d"))
+seq1 <- seq(as.Date("2018-01-01"), as.Date("2018-02-04"), by="days")
+seq2 <- seq(as.Date("2018-01-14"), as.Date("2018-02-17"), by="days")
+
+x <- data.frame(d1=seq1, d2=seq2, stringsAsFactors = FALSE)
